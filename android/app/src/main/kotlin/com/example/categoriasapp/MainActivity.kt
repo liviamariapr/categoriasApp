2ba5eaff7020bc65152de6397db97d99a8da12c7
Livia Maria <liviamariaprates236@gmail.com>
2026-03-02 14:01:40 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/categoriasapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.categoriasapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'screens/categories_screen.dart';
+import 'screens/categories_meals_sreenc.dart';
+import 'utils/app_routes.dart';
+void main () => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'Vamos cozinhar?',
+      theme: ThemeData(
+        useMaterial3: false,
+        colorScheme: ColorScheme.light(
+        primary: Colors.pink,
+        secondary: Colors.amber,
+  ),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',),
+            titleMedium: TextStyle(
+              fontSize: 18,
+              fontFamily: 'Raleway',
+            ),
+            ),
+        ),
+      routes: {
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+      }
+    );
+  } 
+
+}
+
+
